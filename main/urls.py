cdb3c101934ae6d07762ddf08b38cd9af51267b3
Forever Inc dev <redodevs@gmail.com>
2023-07-26 23:17:36 +0300

    save

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from .views import index,services,policy
+from django.urls import path,re_path
+from .views import index,services,policy,sitemap,robots
 
 
 urlpatterns=[
     path('',index,name='home'),
     path('services/',services,name='services'),
-    path('privacy-policy/',policy,name='policy')
+    path('privacy-policy/',policy,name='policy'),
+    path('sitemap/',sitemap,name='sitemap'),
+    re_path(r'^robots\.txt$', robots, name='robots'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import os
+from pathlib import Path
+
 
 # Create your views here.
 def index(request):
@@ -10,3 +14,13 @@ def services(request):
 
 def policy(request):
     return render(request,'main/privacy.html')
+
+
+def robots(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    path=os.path.join(BASE_DIR,'templates','main','robots.txt')
+    response=HttpResponse(open(path).read(),content_type='application/text')
+    return response
+
+def sitemap(request):
+    return render(request,'main/sitemap.xml')
