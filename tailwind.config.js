8e1df373674dad2ef59b045cd5b67d87145aa2b1
Forever Inc dev <redodevs@gmail.com>
2023-07-26 15:19:37 +0300

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: ["./templates/**/*.html", "./node_modules/flowbite/**/*.js"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: "#fff7ed",
+					100: "#ffedd5",
+					200: "#fed7aa",
+					300: "#fdba74",
+					400: "#fb923c",
+					500: "#f97316",
+					600: "#ea580c",
+					700: "#c2410c",
+					800: "#9a3412",
+					900: "#7c2d12",
+					950: "#431407",
+				},
+			},
+		},
+		fontFamily: {
+			body: [
+				"Open Sans",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"system-ui",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+			sans: [
+				"Open Sans",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"system-ui",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+		},
 	},
 	plugins: [require("flowbite/plugin")],
 };
-
