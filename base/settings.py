aa2d0a55a6e4b61ac28b45a21048f7ed77c60388
Forever Inc dev <redodevs@gmail.com>
2023-07-30 15:53:55 +0300

    save

--- base/settings.py
+++ base/settings.py
@@ -5,7 +5,7 @@ from django.contrib.messages import constants as messages
 """
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = get_random_secret_key()
+print( get_random_secret_key())
 """
 
 
