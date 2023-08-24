46a76c2f0ed6fd0b50144b5fcf06ab47b110a96a
Forever Inc dev <redodevs@gmail.com>
2023-08-24 09:43:34 +0300

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,12 @@ module.exports = {
 					200: "#fed7aa",
 					300: "#fdba74",
 					400: "#fb923c",
-					500: "#f97316",
-					600: "#ea580c",
-					700: "#c2410c",
-					800: "#9a3412",
-					900: "#7c2d12",
-					950: "#431407",
+					500: "#ee6435", // Median
+					600: "#ea580c", // Slightly darker
+					700: "#c2410c", // Darker
+					800: "#9a3412", // Even darker
+					900: "#7c2d12", // Very dark
+					950: "#431407", // Extremely dark
 				},
 			},
 		},
